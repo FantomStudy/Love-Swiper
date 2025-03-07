ec123d755ac2bb25259ecbcaed24aa83b35cbe5f
FantomStudy <arzamastsevdaniil@gmail.com>
2025-03-07 22:23:56 +0500

    first_page

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const arrow = document.querySelector(".arrow");
 const hearts = document.querySelectorAll(".heart");
 const tip = document.querySelector(".tip");
 
+let heartCount = 0;
+const updateHeartCounter = () => {
+  heartCount = document.querySelectorAll(".heart.filled").length;
+  switch (heartCount) {
+    case 1:
+      tip.textContent = "Хорошо";
+      break;
+    case 2:
+      tip.textContent = "Продолжай";
+      break;
+    case 3:
+      tip.textContent = "Почти готово..";
+      break;
+    case 4:
+      tip.textContent = "Я люблю ❤️ тебя!";
+      break;
+    case 5:
+      tip.innerHTML = `Идем <a href="#" class="next-link">дальше? <span></span><span></span><span></span></a>`;
+      break;
+    default:
+      tip.textContent = "Увеличивай медленно";
+      break;
+  }
+};
+
 input.addEventListener("input", (e) => {
   const value = parseInt(e.target.value);
   const adjustedAngle = value - 90;
@@ -19,4 +44,7 @@ input.addEventListener("input", (e) => {
       heart.classList.remove("filled");
     }
   });
+  updateHeartCounter();
 });
+
+updateHeartCounter();
